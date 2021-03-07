7b97dd25725ee5521b266910a3ce379b3c7f0e0d
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-03-07 23:29:30 +0300

    css modules lesson14

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+    <header className={s.header}>
+        <img src='https://p1.hiclipart.com/preview/191/668/38/170-dock-icons-blue-two-faces-logo.jpg' />
+    </header>
+    );
+}
+
+export default Header;
