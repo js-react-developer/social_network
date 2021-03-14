320be800f022456240337cafa34dd5e3dbf9e8ac
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-03-14 13:34:47 +0300

    add ref to 2 buttons | lesson 31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,16 +5,28 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
 
-    let dialogsElements = props.state.dialogs.map( d => <DialogItem id={d.id} name={d.name} /> );
-    let messagesElements = props.state.messages.map( m => <Message message={m.message} /> );
+    let dialogsElements = props.state.dialogs.map(d => <DialogItem id={d.id} name={d.name} />);
+    let messagesElements = props.state.messages.map(m => <Message message={m.message} />);
+
+    let newMessageElement = React.createRef();
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                { dialogsElements }
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                { messagesElements }
+                {messagesElements}
+                <div>
+                    <textarea ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={ addMessage }>Send</button>
+                </div>
             </div>
         </div>
     );
