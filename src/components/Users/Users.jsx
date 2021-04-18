ec5495243d0ce58b183ef6702f2d134f389fe9db
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-04-18 18:50:46 +0300

    added pagination of users | lesson 55

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,14 +6,38 @@ import userPhoto from '../../assets/images/user.png'
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
     }
 
     render() {
+
+        let pagesCount = Math.ceil((this.props.totalUsersCount - 11500) / this.props.pageSize);
+
+        let pages = [];
+
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return <div>
+            <div>
+                {pages.map(p => {
+                    return <button className={ this.props.currentPage === p && s.selectedPage }
+                                 onClick={ () => { this.onPageChanged(p) } }>{p}</button>
+                })}
+            </div>
             {
                 this.props.users.map(u => <div>
                     <span>
@@ -37,10 +61,10 @@ class Users extends React.Component {
                         </span>
                         <span>
                             <div>
-                                {/* {u.location.country} */}
+                                {'u.location.country'}
                             </div>
                             <div>
-                                {/* {u.location.city} */}
+                                {'u.location.city'}
                             </div>
                         </span>
                     </span>
