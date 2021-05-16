6874fa2b5b13eb9f3eef834441c6fb120fb4cb0c
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-05-16 14:11:45 +0300

    using componentDidUpdate in ProfileStatus | lesson 74

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -26,12 +26,20 @@ class ProfileStatus extends React.Component {
         })
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
+                        <span onDoubleClick={ this.activateEditMode }>{this.props.status || "-----"}</span>
                     </div>
                 }
                 {this.state.editMode &&
